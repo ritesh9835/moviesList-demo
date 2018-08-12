e9b0bd35e57942c17ea28356f6ff64ef8537244e
Rai Techintro <ritesh@raitechintro.com>
2018-08-12 11:43:46 +0530

    testPush

--- moviesList/Controllers/HomeController.cs
+++ moviesList/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace moviesList.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "test";
 
             return View();
         }
